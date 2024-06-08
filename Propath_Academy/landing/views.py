b00dc9eaa74fadc0dc2aca30bc81205af487fc2c
Sujay-J-Reddy <sujayj1522002@gmail.com>
2024-06-08 13:53:18 +0530

    event photos fixed

--- Propath_Academy/landing/views.py
+++ Propath_Academy/landing/views.py
@@ -5,31 +5,37 @@ from django.views.generic import TemplateView, FormView
 from .forms import EnquiryForm
 from ..academy.models import Event, Stat
 from django.utils.timezone import now
+from zelthy.apps.shared.tenancy.templatetags.zstatic import zstatic
 
 class LandingView(TemplateView):
     template_name = 'landing/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        default_photo = 'https://via.placeholder.com/150?text=No+Image+Found'
+        default_photo = zstatic({"request": self.request},'packages/styles/images/landing/hero1.jpg')
+        context['default_photo_url'] = default_photo
         events_queryset = Event.objects.all().order_by('-date')[:3]
         sample_events = [
-            Event(name="Sample Event 1", date=now().date() - timedelta(days=1), photo=default_photo, details="Details of Sample Event 1"),
-            Event(name="Sample Event 2", date=now().date() - timedelta(days=2), photo=default_photo, details="Details of Sample Event 2"),
-            Event(name="Sample Event 3", date=now().date() - timedelta(days=3), photo=default_photo, details="Details of Sample Event 3"),
+            Event(name="Sample Event 1", date=now().date() - timedelta(days=1), details="Details of Sample Event 1"),
+            Event(name="Sample Event 2", date=now().date() - timedelta(days=2), details="Details of Sample Event 2"),
+            Event(name="Sample Event 3", date=now().date() - timedelta(days=3), details="Details of Sample Event 3"),
         ]
 
+        actual_events = list(events_queryset)
         for i in range(3):
-            context[f'event{i+1}'] = events_queryset[i] if i < len(events_queryset) else sample_events[i]        
+            if i < len(actual_events):
+                context[f'event{i+1}'] = actual_events[i]
+            else:
+                context[f'event{i+1}'] = sample_events[i]
+
         try:
             numbers = Stat.objects.latest('created_at')
         except Stat.DoesNotExist:
-            numbers = Stat(students=0, teachers=0, franchises=0)  # Example sample stats
+            numbers = Stat(students=0, teachers=0, franchises=0)  
 
         context['numbers'] = numbers
 
         return context
-
 
 class CoursesView(TemplateView):
     template_name = 'landing/courses.html'
